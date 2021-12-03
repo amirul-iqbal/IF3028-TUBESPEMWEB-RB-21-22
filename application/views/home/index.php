06eb334f409c3942b90d572836ac05ba04d99d6c
M. Yafi Fahmi <nandagoreh@gmail.com>
2021-12-03 15:54:29 +0700

    setting awal codeigniter

---
+++ application/views/home/index.php
@@ -0,0 +1,1 @@
+<p>Simpel Lapor</p>
---
+++ application/views/templates/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap');
+		body{
+			font-family: 'Poppins', sans-serif;
+			font-weight: 400;
+		}
+	</style>
+</head>
+<body>
