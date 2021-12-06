6221dedc5241c6636cdb7088a0c9a08ee72ff3a4
DodiDevrian <84231458+DodiDevrian@users.noreply.github.com>
2021-12-06 18:10:44 +0700

    Membuat Tampilan Search
    Membuat search bar dan button untuk menambahkan laporan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Lapor</title>
-</head>
-<body>
-    <h1>Ini adalah Homepage</h1>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Lapor</title>
+    </head>
+    <body>
+        <div class="cover">
+            <header>
+                <img src="images/logo.png">
+            </header>
+        <main>
+            <div class="cari">
+                <input type="text" name="cari">
+                <input type="submit" value="Cari">
+            </div>
+            <div class="add">
+                <label>Tambah Laporan/Komentar</label>
+                <a href=""><img src="images/add.png" width="13px" height="13px"></a>
+            </div>
+
+            <h3>Laporan/Komentar Terakhir</h3>
+        </main>
+        </div>
+        <footer>
+            
+        </footer>
+    </body>
 </html>
