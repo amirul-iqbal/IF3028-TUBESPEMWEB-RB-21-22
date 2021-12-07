537706a787d008c7b7e125cf580120a9d48455a3
DodiDevrian <84231458+DodiDevrian@users.noreply.github.com>
2021-12-07 09:58:23 +0700

    Update view-laporan.blade.php
    Membuat halaman detail laporan

--- resources/views/view-laporan.blade.php
+++ resources/views/view-laporan.blade.php
@@ -1,13 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Lapor</title>
-</head>
-<body>
-    <h1>Ini adalah Halaman View Laporan</h1>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/style-view.css">
+        <title>Lapor</title>
+    </head>
+    <body>
+        <div class="cover">
+            <header>
+                <img src="images/logo.png">
+            </header>
+            <main>
+                <h3>Detail Laporan/Komentar</h3>
+                <hr>
+
+                <div class="detail">
+                    <p>
+                      Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.  
+                    </p>
+                </div>
+
+                <div class="lampiran">
+                    <p>Lampiran : </p>
+                    <img src="images/alt-images.jpg" width="200px" height="200px">
+                </div>
+
+                <div class="info">
+                    <div class="combine">
+                        <label class="time">Waktu : (Value)</label>
+                        <label class="aspek">Aspek : (Value)</label>
+                    </div>
+                    <div class="delete">
+                        <a href="">Hapus Laporan/Komentar <img src="images/button.png" width="13px" height="13px"></a>
+                    </div>
+                </div>
+
+                <hr>
+
+            </main>
+        </div>
+        <footer>
+            
+        </footer>
+    </body>
 </html>
