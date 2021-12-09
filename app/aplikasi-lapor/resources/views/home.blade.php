5b8b64e537cc9caf77f43acbf0c30cd265add69b
helmud panggabean <38092137+helmud21@users.noreply.github.com>
2021-12-09 16:30:35 +0700

    memperbaiki tampilan dan juga file style untuk halaman home

--- app/aplikasi-lapor/resources/views/home.blade.php
+++ app/aplikasi-lapor/resources/views/home.blade.php
@@ -2,19 +2,35 @@
 
 @section('konten')
 <div class="konten-home">
+    <div class="cari">
+        <form action="">
+            <input type="text" placeholder="" name="search" id="placeholder-cari">
+            <button type="submit" id="btn-cari"><i class="fa fa-search" id="icon-cari"></i>Cari</button>
+        </form>
+    </div>
+    <div id="home-p1">
+        <p><a href="/buat_laporan">Buat Laporan/Komentar</a><i class="fa fa-plus-square"></i></p>
+    </div>
+
     <div class="list-laporan">
-        <div class="cari">
-            <form action="">
-                <input type="text" placeholder="" name="search" id="placeholder-cari">
-                <button type="submit" id="btn-cari"><i class="fa fa-search" id="icon-cari"></i>Cari</button>
-            </form>
-        </div>
-        <div>
-            <p><a href="/buat_laporan">Buat Laporan/Komentar</a><i class="fa fa-plus-square"></i></p>
-        </div>
-
-        <div class="laporan">
-
+        <p id="home-p2">Laporan/Komentar Terakhir</p>
+        <div class="konten-list-laporan">
+            <div class="isi-laporan">
+                <p>isi laporan</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae sequi asperiores inventore nam, incidunt voluptatem repudiandae impedit, obcaecati soluta quaerat voluptate. Fugiat alias maiores quaerat laborum error tempora eius atque.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis libero perspiciatis totam similique animi! Porro necessitatibus corporis minima obcaecati id doloremque molestias pariatur eligendi, non sed! Inventore pariatur sint adipisci earum cupiditate consequuntur illum facilis repellendus, facere fugit, ratione delectus illo, quis perspiciatis deleniti debitis mollitia. Facilis similique placeat odio?</p>
+            </div>
+            <div class="info-laporan">
+                <div class="info-lampiran">
+                    <p>Info lampiran</p>
+                </div>
+                <div class="info-lainnya">
+                    <ul>
+                        <li>Waktu</li>
+                        <li><a href="#">Lihat Selengkapnya <b>></b></a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </div>
