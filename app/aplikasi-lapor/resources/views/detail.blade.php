2c7970afb58e26a9a0d6ef9f3b87b053713bc3f0
helmud panggabean <38092137+helmud21@users.noreply.github.com>
2021-12-13 10:49:01 +0700

    memperbaiki halaman detail

--- app/aplikasi-lapor/resources/views/detail.blade.php
+++ app/aplikasi-lapor/resources/views/detail.blade.php
@@ -1,29 +1,20 @@
 @extends('templates/main')
 
 @section('konten')
-<div class="konten-home">
-    <div class="cari">
-        <form action="">
-            <input type="text" placeholder="" name="search" id="placeholder-cari">
-            <button type="submit" id="btn-cari"><i class="fa fa-search" id="icon-cari"></i>Cari</button>
-        </form>
-    </div>
-    <div id="home-p1">
+<div class="konten-detail">
+    <div id="detail-p1">
         <p><a href="/buat_laporan">Buat Laporan/Komentar</a><i class="fa fa-plus-square"></i></p>
     </div>
-    <div>
-        <p id="home-p2">Laporan/Komentar Terakhir</p>
-    </div>
-    <div class="list-laporan">
-        <div class="konten-list-laporan">
-            <div class="isi-laporan">
+    <div class="laporan">
+        <div class="konten-laporan">
+            <div class="isi-detail-laporan">
                 <p>{{ $laporan["isi"] }}</p>
             </div>
-            <div class="info-laporan">
-                <div class="info-lampiran">
+            <div class="info-detail-laporan">
+                <div class="info-detail-lampiran">
                     <p>{{ $laporan["lampiran"] }}</p>
                 </div>
-                <div class="info-lainnya">
+                <div class="info-detail-lainnya">
                     <ul>
                         <li>Waktu : {{ $laporan["created_at"] }}</li>
                     </ul>
