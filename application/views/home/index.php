ef0e9a141e36550437dff426bdd875274635a555
Rizaldo Abdulrachman <rizaldo.a.r@gmail.com>
2021-12-15 17:59:35 +0700

    Mengkoding Front-end Sesuai Figma

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -5,6 +5,14 @@
 			rel="stylesheet"
 			href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
 		/>
+		<!-- Font -->
+		<link rel="preconnect" href="https://fonts.googleapis.com" />
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+		<link
+			href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Roboto:wght@300&display=swap"
+			rel="stylesheet"
+		/>
+
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -13,28 +21,36 @@
 	<body>
 		<!-- HEADER -->
 		<div class="Header">
-			<h1>SIMPLE LAPOR!</h1>
+			<div class="container">
+				<div class="row">
+					<div class="col-1">
+						<h2>LAPORIN YUK</h2>
+						<h3>Layanan Aspirasi dan Pengaduan Online Rakyat</h3>
+						<p>Sampaikan Laporan Anda</p>
+						<button type="button">Lapor !</button>
+		<!-- Search box -->
+						<div class="box">
+							<form action=""></form>
+							<input type="text" />
+							<button type="submit"><i class="fa fa-search"></i></button>
+						</div>
+		<!-- Akhir Search box -->
+					</div>
+					<div class="col-2">
+						<img  src="<?php echo $this->config->item('base_url'); ?>/assets/img/logo.png" alt="Logo Report" />
+					</div>
+				</div>
+			</div>
 		</div>
 		<!-- Akhir HEADER -->
-		<!-- Search box -->
-		<div class="box">
-			<form action=""></form>
-			<input type="text" />
-			<button type="submit"><i class="fa fa-search"></i></button>
-		</div>
-		<!-- Akhir Search box -->
-		<div class="buat-laporan-container">
-			<h4>Buat Laporan / Komentar</h4>
-			<button onclick="">
-				<i class="fa fa-plus-square" aria-hidden="true"></i>
-			</button>
-		</div>
+
 		<!-- Komentar Box -->
 		<div class="komentar-body">
-			<h3>Laporan/Komentar Terakhir</h3>
-			<hr />
+			<h3>Laporan Terakhir</h3>
 			<div class="laporan-box-container">
 				<div class="laporan-text">
+					<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, harum.</h4>
+					<p class=ket-waktu>Waktu : 20 - 11 - 2019 20.00</p>
 					<p>
 						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus
 						eveniet nihil commodi odit dolorem non eaque, magni ab laudantium
@@ -45,13 +61,11 @@
 						omnis architecto placeat sit nobis, explicabo consectetur maxime
 						minus magni eveniet rem doloremque quae?
 					</p>
-				</div>
-				<div class="keterangan-container">
-					<div class="lampiran">
+					<div class="ket-gambar">
 						<p>Lampiran: Gambar.jpg</p>
 					</div>
-					<div class="waktu"><p>Waktu : 20 - 11 - 2019 20.00</p></div>
-					<div class="selengkapnya-container">
+				</div>
+				<div class="selengkapnya-container">
 						<a href=""
 							>Lihat Selengkapnya
 							<i
@@ -62,46 +76,128 @@
 						></a>
 					</div>
 				</div>
-				<hr />
 			</div>
-			<div class="laporan-box-container">
-				<div class="laporan-text">
-					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus
-						eveniet nihil commodi odit dolorem non eaque, magni ab laudantium
-						exercitationem accusamus, sint rerum. Illo adipisci consequatur
-						similique deserunt necessitatibus accusantium? Lorem ipsum dolor
-						sit, amet consectetur adipisicing elit. Ratione, architecto?
-						Dignissimos, perferendis? Temporibus, perspiciatis. Adipisci iure
-						omnis architecto placeat sit nobis, explicabo consectetur maxime
-						minus magni eveniet rem doloremque quae?
-					</p>
-				</div>
-				<div class="keterangan-container">
-					<div class="lampiran">
-						<p>Lampiran: Gambar.jpg</p>
-					</div>
-					<div class="waktu"><p>Waktu : 20 - 11 - 2019 20.00</p></div>
-					<div class="selengkapnya-container">
-						<a href=""
-							>Lihat Selengkapnya
-							<i
-								class="fa fa-chevron-right"
-								aria-hidden="true"
-								style="padding-left: 10px"
-							></i
-						></a>
-					</div>
-				</div>
-				<hr />
-			</div>
+			
 		</div>
+		<div class="komentar-body">
+			<div class="laporan-box-container">
+				<div class="laporan-text">
+					<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, harum.</h4>
+					<p class=ket-waktu>Waktu : 20 - 11 - 2019 20.00</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus
+						eveniet nihil commodi odit dolorem non eaque, magni ab laudantium
+						exercitationem accusamus, sint rerum. Illo adipisci consequatur
+						similique deserunt necessitatibus accusantium? Lorem ipsum dolor
+						sit, amet consectetur adipisicing elit. Ratione, architecto?
+						Dignissimos, perferendis? Temporibus, perspiciatis. Adipisci iure
+						omnis architecto placeat sit nobis, explicabo consectetur maxime
+						minus magni eveniet rem doloremque quae?
+					</p>
+					<div class="ket-gambar">
+						<p>Lampiran: Gambar.jpg</p>
+					</div>
+				</div>
+				<div class="selengkapnya-container">
+						<a href=""
+							>Lihat Selengkapnya
+							<i
+								class="fa fa-chevron-right"
+								aria-hidden="true"
+								style="padding-left: 10px"
+							></i
+						></a>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		<div class="komentar-body">
+			<div class="laporan-box-container">
+				<div class="laporan-text">
+					<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, harum.</h4>
+					<p class=ket-waktu>Waktu : 20 - 11 - 2019 20.00</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus
+						eveniet nihil commodi odit dolorem non eaque, magni ab laudantium
+						exercitationem accusamus, sint rerum. Illo adipisci consequatur
+						similique deserunt necessitatibus accusantium? Lorem ipsum dolor
+						sit, amet consectetur adipisicing elit. Ratione, architecto?
+						Dignissimos, perferendis? Temporibus, perspiciatis. Adipisci iure
+						omnis architecto placeat sit nobis, explicabo consectetur maxime
+						minus magni eveniet rem doloremque quae?
+					</p>
+					<div class="ket-gambar">
+						<p>Lampiran: Gambar.jpg</p>
+					</div>
+				</div>
+				<div class="selengkapnya-container">
+						<a href=""
+							>Lihat Selengkapnya
+							<i
+								class="fa fa-chevron-right"
+								aria-hidden="true"
+								style="padding-left: 10px"
+							></i
+						></a>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		<div class="next-back-page-container">
+		<button type="button">Next !</button>
+		<button type="button">Back !</button>
+		</div>
+		
+		<!-- Komentar Box -->
 	</body>
+	<div class="svg-wrapper">
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFD05B" fill-opacity="1" d="M0,192L26.7,181.3C53.3,171,107,149,160,154.7C213.3,160,267,192,320,197.3C373.3,203,427,181,480,149.3C533.3,117,587,75,640,69.3C693.3,64,747,96,800,122.7C853.3,149,907,171,960,176C1013.3,181,1067,171,1120,144C1173.3,117,1227,75,1280,74.7C1333.3,75,1387,117,1413,138.7L1440,160L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path></svg>
+	</div>
+	
+      
+	<footer >
+	
+    </footer>
 </html>
 <style>
+	body {
+		
+	}
+	* {
+		font-family: Roboto;
+	}
+	.row {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		margin: 25px 0;
+	}
+	.col-1 {
+		flex-basis: 40%;
+		position: relative;
+		margin: 50px;
+	}
+	.col-1 h2 {
+		font-size: 54px;
+	}
+	.col-1 h3 {
+		font-size: 30px;
+		color: #707070;
+		font-weight: 100;
+		margin: 20px 0 10px;
+	}
+	.col-1 p {
+		font-size: 16px;
+		color: #b7b7b7;
+		font-weight: 100px;
+	}
+
 	Header {
 		padding: 2rem 1rem;
 	}
+
 	.Header h1 {
 		text-align: center;
 	}
@@ -110,10 +206,9 @@
 		justify-content: center;
 	}
 	.box input[type="text"] {
-		width: 50%;
+		width: 100%;
 		padding: 20px 20px;
-		margin: 8px 0;
-		box-sizing: border-box;
+		margin: 10px 0;
 	}
 	.box button {
 		float: right;
@@ -121,13 +216,13 @@
 		margin-top: 8px;
 		margin-bottom: 8px;
 		margin-right: 16px;
-		background-color: white;
+		background-color: #59c5ec;
 		font-size: 17px;
 		border-color: black;
 		cursor: pointer;
 	}
 	.box button:hover {
-		background: #ccc;
+		background: #ffd05b;
 	}
 	div.buat-laporan-container {
 		display: flex;
@@ -153,7 +248,93 @@
 		padding-right: 300px;
 	}
 	div.selengkapnya-container {
+		padding-bottom: 5px;
+		padding-left  : 1350px;
+	}
+	div.selengkapnya-container a {
+		color : #b7b7b7;
+		text-decoration: none;
+	}
+	button {
+		width: 140px;
+		border: 0;
+		padding: 12px 10px;
+		outline: none;
+		color: #fff;
+		background: #59c5ec;
+		cursor: pointer;
+	}
+	button:hover {
+		background: #ffd05b;
+	}
+	.button:active {
+		background-color: #3e8e41;
+		box-shadow: 0 5px #666;
+		transform: translateY(4px);
+	}
+	.col-1::after {
+		content: "";
+		width: 10px;
+		height: 57%;
+		background: #ffd05b;
+		position: absolute;
+		left: -40px;
+		top: 8px;
+	}
+	.col-2{
+		position: relative;
+		flex-basis: 20%;
 		display: flex;
-		padding-top: 15px;
+		align-items: center;
+	}
+	.komentar-body{
+		padding-top: 50px
+	}
+	.komentar-body h3 {
+		text-align : center;
+		font-size: 30px;
+		color: black;
+		font-weight: bold;
+		
+	}
+	.laporan-box-container {
+		border: 1px solid;
+		box-shadow: 5px 10px #888888;
+	}
+	.ket-waktu {
+		font-size: 12px;
+		color: #b7b7b7;
+		font-weight: bold;
+		margin: 10px 0 5px;
+	}
+	.ket-gambar {
+		font-size: 12px;
+		color: #b7b7b7;
+		font-weight: bold;
+		margin: 10px 0 5px;
+	}
+	.laporan-text h4 {
+		font-size: 20px;
+		color: black;
+		text
+		font-weight: 900;
+		margin: 10px 0 5px;
+	}
+	.laporan-text {
+		padding: 5px;
+	}
+	.next-back-page-container {
+		padding: 2rem;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+	}
+	footer p{
+		background-color:#ffd05b;
+	}
+	.svg-wrapper {
+		position: absolute;
+		left:0;
+		width: 100%;
 	}
 </style>
