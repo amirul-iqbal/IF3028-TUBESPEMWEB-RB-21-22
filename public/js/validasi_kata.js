52275859ae980a77e5815f01fe340b19295d51d0
Tri Aji Bagaskara <90993802+Aji214@users.noreply.github.com>
2021-12-23 15:01:45 +0700

    Update validasi_kata.js

--- public/js/validasi_kata.js
+++ public/js/validasi_kata.js
@@ -1,23 +1,28 @@
-var form = document.getElementById("form");
-var aspek = document.getElementById("aspek");
-var lampiran = document.getElementById("lampiran");
-var isi = document.getElementById("isi");
-
-
-    form.addEventListener('submit', (e) => {
-        if 
-        (
-            lampiran.value == "" &&
-            isi.value == "" &&
-            aspek.value == "false"
-        ) 
-        {
-            alert("Pastikan ;anda mengisi semuanya");
-            e.preventDefault();
-        } 
-        else if (isi.length() < 20) 
-        {
-            alert("Minimal untuk panjang komentar adalah 20");
-            e.preventDefault();
+function createReports() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+       if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("container").innerHTML =
+        this.responseText;
         }
-    });
+    };
+    xhttp.open('POST','/',true);
+    xhttp.send();
+}
+
+function validasi(){
+    var laporan = document.forms["formlapor"]["laporan"].value;
+    laporan = laporan.split(" ");
+    var aspek = document.forms["formlapor"]["aspek"].value;
+    var lampiran = document.forms["formlapor"]["lampiran"].value;
+ 
+    if (laporan.length < 20){
+        alert("Isi laporan harus lebih dari 20 kata");
+        return false;
+    }
+    if (aspek == "" || aspek == null){
+        alert("Aspek harus Dipilih");
+        return false;
+    }
+createReports();
+}
